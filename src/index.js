a3901653034418503fb6696d987d6ae08c658285
Alden  Sullivan <aw.sullivan17@gmail.com>
2023-11-02 17:48:22 -0400

    Temp

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './extras/reportWebVitals';
 import { sendToVercelAnalytics } from './extras/vitals';
 
-ReactDOM.render(
-  <App />
-  ,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 reportWebVitals(sendToVercelAnalytics);
